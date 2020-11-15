3d9279924fd3cd3f1a919eb473cba89e666abd89
Samuel Fabrizio <samuelfabrizio@gmail.com>
2020-11-15 23:17:25 +0100

    [ADD] store speed cmd and re send it as well #1
         - improved parse & checking using Json

--- broker.js
+++ broker.js
@@ -10,36 +10,48 @@ const defaultCmd = {
     spd: 200
 };
 
-//let lastCommandBuffer = Buffer.from(JSON.stringify(defaultCmd));
-let lastCommandBuffer = false;
-let lastClient;
+let lastFx = null;
+let lastSpd = null;
+let lastClient = null;
 
-server.on('clientConnected', function(client) {
+server.on('clientConnected', function (client) {
     console.log('client connected', client.id);
-    if (!lastCommandBuffer) {
+    if (!lastFx) {
         return;
     }
     lastClient = client;
-    console.log("waiting 7s to re publish cmd");
-    setTimeout(sendLastCmd, 7000);
+    console.log("waiting 5s to re publish cmd");
+    setTimeout(sendLastCmd, 5000);
 });
 
 function sendLastCmd() {
     console.log("re publishing last cmd");
     server.publish({
         topic: 'myhome',
-        payload: lastCommandBuffer
+        payload: lastFx
+    }, lastClient);
+    server.publish({
+        topic: 'myhome',
+        payload: lastSpd
     }, lastClient);
 
 }
 
 // fired when a message is received
-server.on('published', function(packet, client) {
+server.on('published', function (packet, client) {
     const msgString = packet.payload.toString();
     console.log('got published: ', msgString);
-    if (msgString.includes("{'cmd':'fx'")) {
-        console.log('storing cmd');
-        lastCommandBuffer = packet.payload;
+    if (msgString.includes("{'cmd") || msgString.includes('{"cmd')) {
+        const payloadObj = JSON.parse(msgString);
+        if (!payloadObj.cmd) {
+            return;
+        }
+        console.log('setting last cmd as: ', payloadObj);
+        if (payloadObj.cmd == "fx") {
+            lastFx = JSON.stringify(payloadObj);
+        } else if (payloadObj.cmd == "spd") {
+            lastSpd = JSON.stringify(payloadObj);
+        }
     }
 
 });
@@ -48,5 +60,5 @@ server.on('ready', setup);
 
 // fired when the mqtt server is ready
 function setup() {
-    console.log('Mosca server is up and running');
+    console.log('Mosca server is up and running and cool');
 }
